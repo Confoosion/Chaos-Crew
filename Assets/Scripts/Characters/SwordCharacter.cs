ab32dfbea855f265a37a60898bdcc2c66943b10f
Jake <jakesales@gmail.com>
2026-04-28 14:29:10 -0700

    Added dual wield (2 sided attack) for Swordsman
    This is his 3rd upgrade

--- Assets/Scripts/Characters/SwordCharacter.cs
+++ Assets/Scripts/Characters/SwordCharacter.cs
@@ -3,10 +3,18 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Characters/Sword Character")]
 public class SwordCharacter : CharacterSO
 {
+    [SerializeField] private bool dualWield = false;
+
     public override void UseWeapon(Transform origin, PlayerAttack playerAttack)
     {
         float direction = (origin.localScale.x == 1) ? -1f : 1f;
         GameObject atk = Instantiate(attackObject, origin.position + new Vector3(direction, 0f, 0f), Quaternion.identity, origin);
         atk.GetComponent<MeleeAttack>().SetData(attackPower, attackDuration);
+
+        if(dualWield)
+        {
+            atk = Instantiate(attackObject, origin.position + new Vector3(-direction, 0f, 0f), Quaternion.Euler(0f, 0f, 180f), origin);
+            atk.GetComponent<MeleeAttack>().SetData(attackPower, attackDuration);
+        }
     }
 }
